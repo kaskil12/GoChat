1754e5a52ec982de916a60d9c49b7ddd6bf93ccc
Kasper Kilde <kasperkilde2@gmail.com>
2025-06-01 23:28:17 +0200

    Update: Design better

--- gochat/src/components/Chatbox.tsx
+++ gochat/src/components/Chatbox.tsx
@@ -8,13 +8,13 @@ export default function Chatbox(){
     }
     return(
         <>
-            <div className="flex flex-col h-[100wh] overflow-scroll">
+            <div className="flex flex-col h-[100wh] overflow-scroll pb-40">
                 {chat.map((item, index) => {
                     return (
                     <div key={index} className="border rounded-md p-4 mt-2 flex bg-[#4E545C]">
                         <div className="flex flex-col">
                             <p className="font-bold ml-10 text-[#000401] text-xl">User</p>
-                            <div className="flex">
+                            <div className="flex break-all">
                                 <img src="/profile.jpg" alt="Profile" className="w-8 h-8 rounded-full mr-2" />
                                 <p>{item}</p>
                             </div>
@@ -23,9 +23,21 @@ export default function Chatbox(){
                     )
                 })}
             </div>
-            <div className="fixed bottom-0 flex items-center justify-center w-full">
-                <div className="flex">
-                    <input className="bg-[#8D9797] rounded-md w-200 h-10" value={text} onChange={e=>setText(e.target.value)} placeholder="Message..." onKeyDown={e=>{if(e.key === "Enter"){addChat(text)}}}></input>
+            <div className="fixed bottom-0 flex items-center justify-center w-full break-words field-sizing-content">
+                <div className="flex w-full">
+                    <textarea
+                        className="bg-[#8D9797] rounded-md max-w-full max-h-50 min-h-20 mb-7 shadow-md border border-[#000000] inset-shadow-sm inset-shadow-indigo-500 w-full" 
+                        value={text}
+                        onChange={e => setText(e.target.value)}
+                        placeholder="Message..."
+                        onKeyDown={e => {
+                            if (e.key === "Enter" && !e.shiftKey) {
+                                addChat(text);
+                                setText("");
+                            }
+                        }}
+                        rows={1}
+                    />
                 </div>
             </div>
         </>
